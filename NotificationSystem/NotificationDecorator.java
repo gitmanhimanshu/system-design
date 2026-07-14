eaa020b4911a7e5b92b20a8bd4eae812ce7ca330
himanshu <himanshuyada70@gmail.com>
2026-07-14 06:09:06 +0530

    fix: change notification field to protected in NotificationDecorator for subclass access

--- NotificationSystem/NotificationDecorator.java
+++ NotificationSystem/NotificationDecorator.java
@@ -1,5 +1,5 @@
 abstract class NotificationDecorator implements INotification{
-    private INotification notification;
+    protected INotification notification;
     public NotificationDecorator(INotification n){
         this.notification=n;
     }
